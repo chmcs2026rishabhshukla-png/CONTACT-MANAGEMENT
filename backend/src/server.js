f911401a1a5e85e0b00db4d12a8b0174749cd4d1
RISHABH <chmcs2026.rishabh.shukla@gmail.com>
2026-03-07 10:29:43 +0530

    updated code

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import contactRoutes from "./Routes/contactRoutes.js";
+import cors from "cors";
 
+app.use(cors());
 dotenv.config();
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 connectDB();
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://contact-management-07i9.onrender.com/api"
+  baseURL: "https://contact-management-07i9.onrender.com  "
 });
 
 export default api;
